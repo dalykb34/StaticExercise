f1f518b975b7e1dd58126c026090ab1700d05fda
Kevin Daly <dalykb34@gmail.com>
2020-09-04 10:55:43 -0500

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var celsius = TempConverter.FahrenheitToCelsius(72);
+            Console.WriteLine($"{celsius} degrees");
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(32);
+            Console.WriteLine($"{fahrenheit} Degrees");
+        
+        }
+
+    }
+}
